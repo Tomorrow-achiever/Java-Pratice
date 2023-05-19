cd7aa7e911e2e5ce0b8e5653201abb3fd43056ea
shankar23112002 <72605597+shankar23112002@users.noreply.github.com>
2023-05-19 22:09:42 +0530

    changed to new

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,15 +2,5 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Grater London");
 
-        System.out.println("Hello World");
-
-        System.out.println("Welcome India");
-        System.out.println("Welcome Sts");
-        System.out.println("Welcome git");
-        System.out.println("adding in sts branches new");
-
-
-
-
     }
 }
