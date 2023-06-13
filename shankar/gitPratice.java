24d560dd9bd8100088cfa5c0e2af6d2b4ce73755
shankar23112002 <72605597+shankar23112002@users.noreply.github.com>
2023-06-13 22:27:29 +0530

    added variable

--- shankar/gitPratice.java
+++ shankar/gitPratice.java
@@ -3,5 +3,7 @@ package shankar;
 public class gitPratice {
     public static void main(String[] args) {
         System.out.println("this is for git");
+        int x=7;
+        System.out.println(x);
     }
 }
